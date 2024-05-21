e26691c91b0fa1578a4495e09fe35edc26aad56f
stivenga <stivenguerreroarbelaez@gmail.com>
2024-05-20 20:18:27 -0500

    Segundo commit, creacion API, Conexion mongoDB, variables de entorno

---
+++ api.js
@@ -0,0 +1,37 @@
+var mongoose = require("mongoose");
+var express = require("express");
+var router = express.Router();
+let environment = null;
+
+if (!process.env.ON_RENDER) {
+    console.log("Cargando variables de entorno desde archivo");
+    const env = require('node-env-file');
+    env(__dirname + '/.env');
+}
+
+environment = {
+    DBMONGOUSER: process.env.DBMONGOUSER,
+    DBMONGOPASS: process.env.DBMONGOPASS,
+    DBMONGOSERV: process.env.DBMONGOSERV,
+    DBMONGO: process.env.DBMONGO,
+};
+
+var query = 'mongodb+srv://' + environment.DBMONGOUSER + ':' + environment.DBMONGOPASS + '@' + environment.DBMONGOSERV + '/' + environment.DBMONGO + '?retryWrites=true&w=majority';
+
+
+const db = (query);
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function (error) {
+    if (error) {
+        console.log("Error!" + error);
+    } else {
+        console.log("Se ha conectado con la base de datos exitosamente");
+    }
+});
+
+module.exports = router;
